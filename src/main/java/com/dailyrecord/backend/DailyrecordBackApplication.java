5661f8af88e2258d131b182d15e667114ddc3226
hyundonglee <haksa1008@gmail.com>
2025-01-03 01:51:29 +0900

    feat: Django 프로젝트를 Spring Boot로 변경 및 초기 설정 완료
    - Spring Boot 초기 설정 파일 추가
    - 데이터베이스 연동(application.properties) 구성
    - Maven Wrapper 및 의존성 설정 추가

---
+++ src/main/java/com/dailyrecord/backend/DailyrecordBackApplication.java
@@ -0,0 +1,13 @@
+package com.dailyrecord.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyrecordBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyrecordBackApplication.class, args);
+	}
+
+}
